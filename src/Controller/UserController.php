2329efd71bb6ea64febada944d922ae4a22faa9b
Wakonda <amatukami@hotmail.fr>
2019-07-27 11:41:31 +0200

    Quote user issues

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -64,7 +64,7 @@ class UserController extends Controller
 	{
 		$entityManager = $this->getDoctrine()->getManager();
 		if(!empty($username))
-			$entity = $entityManager->getRepository(User::class)->findOneByName(["username" => $username]);
+			$entity = $entityManager->getRepository(User::class)->findOneBy(["username" => $username]);
 		else
 			$entity = $tokenStorage->getToken()->getUser();
 
